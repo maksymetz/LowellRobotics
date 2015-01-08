7bb64d89c79ccbd876bd67ca41ec12ed88b77219
maksymetz <maksymetz@hotmail.com>
2015-01-07 19:05:36 -0500

    Initial upload 1
    Initial upload 1

---
+++ bin/foot.php
@@ -0,0 +1,164 @@
+<?php
+echo ('
+</td>
+<!-- **** SIDEBAR AREA **** -->
+<td valign="top" style="width:205px;">
+
+
+
+<div style="border:#600000 1px solid;width:200px;">
+<h3>Upcoming Events</h3>
+<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=7i7gq5oo153os18vjfb90ukjgs%40group.calendar.google.com&amp;color=%230F4B38&amp;ctz=America%2FNew_York" style=" border-width:0 " width="200" height="400" frameborder="0" scrolling="no"></iframe>
+</div>
+
+<div style="width:200px;margin-right:auto;margin-top:10px;">
+<h3>Signature Sponsors</h3>
+<br/>
+<a href="http://www.grfoundation.org/lowell"><img src="/img/sponsors/lowellCommFund.gif" style="max-width:187px;" alt="Lowell Community Fund" /></a>
+<br/>
+<a href="http://www.wbtooling.com/"><img src="/common/whitesbridge.jpg" style="max-width:187px;" alt="White\'s Bridge Tooling" /></a>
+<br/>
+<a href="http://www.dairydiscovery.com/"><img src="/pages/about/sponsors/img/Dairy-Discovery.png" style="max-width:187px;" alt="Swiss Lane Farms" /></a>
+<br/>
+<a href="http://www.hooperprinting.com/"><img src="/pages/about/sponsors/img/HooperLogo.png" style="max-width:187px;" alt="Hooper Printing" /></a>
+<br/>
+<a href="http://www.vanderzielmachinery.com/"><img src="/pages/about/sponsors/img/Banner-Logo.png" style="max-width:187px;" alt="VanderZiel Machinery" /></a>
+</div>
+<br />
+
+
+
+ ');
+
+//<table style="width:200px;background:#dce6f2;">
+
+//Define Array of Events
+//$events = array(		0 => '<b>Jan. 5 Kickoff</b><br />GVSU 8:00AM - 5:00',
+//				1 => '<b>Mar. 1 Competition</b><br />Traverse City',
+//				2 => '<b>Mar. 22 Competition</b><br />GVSU',
+//				3 => '<b>Apr. 11 State Competition</b><br /><a href="http://maps.google.com/maps?hl=en&rlz=1C1CHKZ_enUS437US437&ion=1&bav=on.2,or.r_gc.r_pw.r_cp.r_qf.,cf.osb&biw=1024&bih=651&um=1&ie=UTF-8&q=EMU+Convocation+Center&fb=1&gl=us&hq=EMU+Convocation+Center&cid=0,0,3556452549635800831&ei=D-Z5T4mTOeTSmAX87bTpDw&sa=X&oi=local_result&ct=image&ved=0CAsQ_BI">EMU</a> 9:00AM - 6:30PM');
+//				
+//	
+//Loop through events 
+//$j = 0;
+//for($i=0; $i<=sizeof($events); $i++)
+//{
+//	switch($j)
+//	{
+//		case 0:
+//			echo("<tr><td class='eventa'>".$events[$i]."</td></tr>\n");
+//			$j=1;
+//			break;
+//		case 1:
+//			echo("<tr><td class='eventb'>".$events[$i]."</td></tr>\n");
+//			$j=0;
+//			break;
+//	}
+//}
+//
+//echo ('
+//</table>
+//</div>');
+echo ('<br />');
+
+$url = (!empty($_SERVER['HTTPS'])) ? "https://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] : "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+
+
+//Display Member Menu if logged in//
+if(!$user->data['user_id'] == ANONYMOUS)
+{
+	echo('
+	<div style="border:#600000 1px solid;width:200px;">
+	<h3>Members Sign-in</h3>
+	<form method="post" action="/forum/ucp.php?mode=login">
+	<fieldset>
+	<label>Username:</label> <input type="text" name="username" id="username" /><br />
+	<label>Password:</label> <input type="password" name="password" id="password" /><br />
+	<p>Remember Me?:</p> <input type="checkbox" name="autologin" id="autologin"  /><br />
+	<input type="submit" value="Log In" name="login" />
+	<input type="hidden" name="redirect" value="'.$url.'" />
+	</fieldset>
+	</form>
+	</div>
+	');
+}
+else
+{
+	if (2==1)
+	{
+
+	echo ('
+	<div style="border:#600000 1px solid;width:200px;">
+	<h3>Members</h3>
+	<div id="loggedInBar">
+	<div id="userInfo">
+	Welcome: '.$user->data['username'].'
+	</div>
+	<div id="userMenu">
+	<a href="/forum/index.php">Forum</a>
+	<br>
+    <a title="See You Later" href="' . append_sid("{$phpbb_root_path}ucp.$phpEx", 'mode=logout', true, $user->session_id). '">Log out</a>
+	</div>
+	</div>
+	</div>
+	');
+	}
+
+}
+
+echo('
+
+<div style="margin-left:auto;">
+<a href="http://validator.w3.org/check?uri=referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="28" width="70" /></a>
+<img style="border:0;width:70px;height:28px" src="/common/css.png" alt="Valid CSS" />
+</div>
+<hr />
+
+<div style="margin-left:auto;">
+<a href="#"><img src="/common/FRC-ExcelAward-2012.gif" alt="2012 Website Excelence"/></a>
+</div>
+<hr />
+
+    </td>
+  </tr>
+</table>
+</div>
+<div id="footer"><!-- ***** FOOTER ***** -->
+<table>
+  <tr>
+    <td style="border-right:#ffffff 1px solid;text-align:center;">
+      <a href="/index.php">Home Page</a>
+    </td>
+    <td style="border-right:#ffffff 1px solid;text-align:center;">
+      <a href="/pages/about/team/">About Us</a>
+    </td>
+    <td style="border-right:#ffffff 1px solid;text-align:center;">
+      <a href="http://www.usfirst.org/">FIRST</a>
+    </td>
+    <td style="border-right:#ffffff 1px solid;text-align:center;">
+      <a href="/pages/news/2014/">Team News</a>
+    </td>
+    <td style="border-right:#ffffff 1px solid;text-align:center;">
+      <a href="/pages/photo_gallery/pg_2010_build.php">Photo Gallery</a>
+    </td>
+    <td style="text-align:center;">
+      <a href="/pages/links/external_links.php">External Links</a>
+    </td>
+  </tr>
+</table>
+<hr />
+<span style="font-family:arial;font-size:12px;color:#d0d0d0;text-align:center;">This website is entirely student designed and maintained.  <a href="/pages/about/website">Read more</a><br />
+Copyright &#169; '.date("Y").' Lowell FIRST Robotics Team 3234</span>
+
+</div>
+
+
+
+
+
+
+</div>
+</body>
+</html>
+');
+?>
---
+++ bin/head.php
@@ -0,0 +1,150 @@
+<?php
+define("BASE_PATH",$_SERVER['DOCUMENT_ROOT']);
+
+define('IN_PHPBB', true);
+$phpbb_root_path = '/forum/';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+
+require_once("/bin/mobile.php");
+
+global $css, $js, $onload, $title; // get page defined variables
+
+if(!isset($title))
+{
+	$title="Red Arrow Robotics";
+}
+
+echo ('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+	<title>'.$title.' - FRC #3234</title>
+	<link rel="shortcut icon" href="/common/favicon.ico" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Red Arrows FIRST Robotics Team 3234 from Lowell High School, Michigan." />
+	<meta name="keywords" content="lowell,robotics,first,3234,robotics,frc,red arrows,michigan,competition" />
+	<meta name="author" content="Lowell Robotics student website team." />
+	<meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1" />
+	');
+	if(!$mobile)
+	{		echo('<link rel="stylesheet" type="text/css" href="/common/master.css" />');
+	} else {
+		echo('<link rel="stylesheet" type="text/css" href="/common/mobile.css" />');
+	}
+	
+	if(isset($css))
+	{
+		for($i=0; $i<count($css); $i++)
+		{
+			echo('<link rel="stylesheet" type="text/css" href="'.$css[$i].'" />
+			');
+		}
+	}  
+	echo ('
+	<script src="/common/dropdown.js" type="text/javascript"></script>
+	');
+	if(isset($js))
+	{
+		for($i=0; $i<count($js); $i++)
+		{
+			echo('<script src="'.$js[$i].'" type="text/javascript"></script>');
+		}
+	}
+  
+echo ('
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push([\'_setAccount\', \'UA-32347921-1\']);
+  _gaq.push([\'_trackPageview\']);
+
+  (function() {
+    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;
+    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';
+    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</head>
+');
+
+if(isset($onload))
+  {
+	echo('<body onload='.$onload.'>');
+  } else {
+echo('<body>');
+}
+
+echo('
+<div id="center"><!-- This division centers the page in accordance with valid XHTML -->
+<!-- The BODY of the page design is comprised of 4 divisions, each labeled HEADER, NAVBAR, PAGE, and FOOTER, respectively -->
+<!-- ***** HEADER ***** -->
+<div id="header"><table>
+  <tr>
+    <td style="text-align:left;"><img src="/common/banner.png" style="height:180px; width:700px;" alt="Lowell Robotics" /></td>
+	<td style="min-width:80px;">&nbsp;&nbsp;</td>
+    <td style="text-align:right;"><a href="http://www.usfirst.org/" class="FIRSTLOGO"><img src="/common/FIRST.png" style="width:165px;height:170px;" alt="FIRST Logo" /></a></td>
+  </tr>
+</table>
+</div><!-- ***** NAVBAR ***** --><div id="navbar">
+<table id="dropdown">
+  <tr>
+    <td style="border-right:#000000 1px solid;">
+      <a href="/" onmouseover="mopen(\'m2\')" onmouseout="mclosetime()">Home</a>
+      <div id="m2" onmouseover="mcancelclosetime()" onmouseout="mclosetime()" style="height:0px;"></div>
+    </td>
+    <td style="border-right:#000000 1px solid;">
+      <a href="/pages/about/team" onmouseover="mopen(\'m3\')" onmouseout="mclosetime()">About Us</a>
+      <div id="m3" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+        <a href="/pages/about/team">Our Team</a>
+        <a href="/pages/about/sponsors">Our Sponsors</a>
+        <a href="/pages/about/robot">Our Robots</a>
+      </div>
+    </td>
+	<td style="border-right:#000000 1px solid;">
+		<a href="/pages/first" onmouseover="mopen(\'m4\')" onmouseout="mclosetime()">About FIRST</a>
+			<div id="m4" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+				<a href="http://www.usfirst.org/">FIRST</a>
+				<a href="http://www.firstinmichigan.org/">FIRST in Michigan</a>
+			</div>
+		</td>
+    <td style="border-right:#000000 1px solid;">
+		<a href="/pages/news/2014/" onmouseover="mopen(\'m5\')" onmouseout="mclosetime()">Team News</a>
+			<div id="m5" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+				<a href="/pages/news/2014/">2014 Season</a>
+				<a href="/pages/news/2013/">2013 Season</a>
+				<a href="/pages/news/2012/">2012 Season</a>
+				<a href="/pages/news/2011/">2011 Season</a>
+				<a href="/pages/news/2010/">2010 Season</a>
+			</div>
+    </td>
+	<td style="border-right:#000000 1px solid;">
+		<a href="/pages/photo_gallery/pg_2014_build.php" onmouseover="mopen(\'m6\')" onmouseout="mclosetime()">Photo Gallery</a>
+			<div id="m6" onmouseover="mcancelclosetime()" onmouseout="mclosetime()">
+				<a href="/pages/photo_gallery/pg_2014_build.php">2014 Photos</a>
+				<a href="/pages/photo_gallery/pg_2013_build.php">2013 Photos</a>
+				<a href="/pages/photo_gallery/pg_2010_build.php">2010 Photos</a>
+			</div>
+    </td>
+	
+  
+    <td>
+      <a href="/pages/links/external_links.php" onmouseover="mopen(\'m7\')" onmouseout="mclosetime()">External Links</a>
+      <div id="m7" onmouseover="mcancelclosetime()" onmouseout="mclosetime()" style="height:0px;"></div>
+    </td>
+  </tr>
+</table>
+<div style="clear:both"></div>
+</div>
+
+<!-- ********* PAGE ********* --><div id="pagebody">
+<table>
+  <tr>
+    <td style="width:785px;border-right:#000000 1px solid;padding:0px;padding:5px;" valign="top">');
+
+?>
