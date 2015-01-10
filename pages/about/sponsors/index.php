253e876591f82b272dddb90870e0fe61029736a8
maksymetz <maksymetz@hotmail.com>
2015-01-10 12:34:21 -0500

    LowellRobotics/pages/about/sponsors index.php
    Try again

--- pages/about/sponsors/index.php
+++ pages/about/sponsors/index.php
@@ -4,37 +4,36 @@ require_once('/bin/head.php');
 ?>
 
 <!-- **** MAIN AREA **** -->
-<h2 class="spon">Our Sponsors</h2>
+<h2 class "spon">Our Sponsors</h2>
 <br>
-<h3 class="spon">Lowell Community Fund</h3>
+<h3 class "spon">Lowell Community Fund</h3>
 <p>The Lowell Area Community Fund gives grants to organizations that assist in fulfilling its mission to assure community cooperation and participation that supports a healthy, dynamic community.</p>
 <br>
-<h3 class="spon">Lowell Area Schools</h3>
+<h3 class "spon">Lowell Area Schools</h3>
 <p>Thanks to Lowell Schools for giving us a work area to build our robot!</p>
 <br>
-<h3 class="spon">White's Bridge Tooling</h3>
+<h3 class "spon">White's Bridge Tooling</h3>
 <p>For over 25 years, White's Bridge Tooling, Inc., based in Lowell, MI, has turned unique manufacturing problems into custom solutions.</p>
 <br>
-<h3 class="spon">Enworks</h3>
+<h3 class "spon">Enworks</h3>
 <p>A company that makes office furniture for offices.</p>
 <br>
-<h3 class="spon">Perceptive Concepts</h3>
+<h3 class"spon">Perceptive Concepts</h3>
 <p>Perceptive Concepts designs mechanical machinery like conveyors, product elevators, filling equipment, palletizing systems (robotic and traditional), robotic welding and / or assembly systems and assembly fixtures.</p>
 <br>
-<h3 class"spon">Bernards Ace Hardware</h3>
+<h3 class "spon">Bernards Ace Hardware</h3>
 <p>This hardware store donates tools and offers us discounts.</p>
 <br>
-<h3 class"spon">Metric Manufacturing</h3>
+<h3 class "spon">Metric Manufacturing</h3>
 <p>They do CAD design and manufacturing.</p>
 <br>
-<h3 class"spon">Orchard Hills Golf Course</h3>
-<p> Located in Shelbyville, MI.</p>
+<h3 class "spon">Orchard Hills Golf Course</h3>
+<p>Located in Shelbyville, MI.</p>
 <br>
-<h3 class"spon">Individual Doners</h3>
+<h3 class "spon">Individual Doners</h3>
 Deb Ellinger
 <br>
-<h3 class"spon">Orchard Hills Golf Course</h3>
-<p> Located in Shelbyville, MI.</p>
+
 
 <?php
 require_once('/bin/foot.php');
