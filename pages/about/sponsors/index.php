f568586f3c26225b5a6cbdbedaba26447d6e08ed
mark <mew2011@live.com>
2015-01-10 11:56:09 -0500

    Update index.php

--- pages/about/sponsors/index.php
+++ pages/about/sponsors/index.php
@@ -10,7 +10,7 @@ require_once('/bin/head.php');
 <p>The Lowell Area Community Fund gives grants to organizations that assist in fulfilling its mission to assure community cooperation and participation that supports a healthy, dynamic community.</p>
 <br>
 <h3 class="spon">Lowell Area Schools</h3>
-<p>Thanks to Lowell Schools for giveing us a work area to build are robot!</p>
+<p>Thanks to Lowell Schools for giving us a work area to build our robot!</p>
 <br>
 <h3 class="spon">White's Bridge Tooling</h3>
 <p>For over 25 years, White's Bridge Tooling, Inc., based in Lowell, MI, has turned unique manufacturing problems into custom solutions.</p>
@@ -18,20 +18,19 @@ require_once('/bin/head.php');
 <h3 class="spon">Enworks</h3>
 <p>A company that makes office furniture for offices.</p>
 <h3 class="spon">Perceptive Concepts</h3>
-<p>Perceptive Concepts Design Macanical machiner for manafacur compines like Conveyors,Product elevators,Filling equipment,Palletizing systems (robotic and traditional),Robotic welding and / or assembly systems,Assembly fixtures,Vibratory bowl feed high speed assembly equipment
-Palletized power and free BOSCH / LANCO style systems "Automation by design".</p>
+<p>Perceptive Concepts designs mechanical machinery like conveyors, product elevators, filling equipment, palletizing systems (robotic and traditional), robotic welding and / or assembly systems and assembly fixtures.</p>
 <br>
 <h3 class"spon">Bernards Ace Hardware</h3>
-<p>they are a hardware store hear in lowell they have donated tools to us and have given us discounts in the past</p>
+<p>This hardware store donates tools and offers us discounts.</p>
 <br>
 <h3 class"spon">Metric Manufacturing</h3>
-<p>they do cad dising and manufactring jobs</p>
+<p>They do CAD design and manufacturing./p>
 <br>
 <h3 class"spon"<doners</h3>
 <p>Deb Ellinger</P>
 <br>
 <h3 class"spon"<Orchard Hills Golf Course</h3>
-<p>they are a golf course loacated in  Shelbyville, MI </p>
+<p> Located in Shelbyville, MI.</p>
 
 <?php
 require_once('/bin/foot.php');
