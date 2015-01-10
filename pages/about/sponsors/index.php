e0f294aa0a20e9b4e50c193edf89d0340383f55f
mark <mew2011@live.com>
2015-01-10 11:43:24 -0500

    Update index.php

--- pages/about/sponsors/index.php
+++ pages/about/sponsors/index.php
@@ -9,20 +9,29 @@ require_once('/bin/head.php');
 <h3 class="spon">Lowell Community Fund</h3>
 <p>The Lowell Area Community Fund gives grants to organizations that assist in fulfilling its mission to assure community cooperation and participation that supports a healthy, dynamic community.</p>
 <br>
-<h3 class="spon">Autodie</h3>
-<img src="/img/sponsors/autodie/autodie.png" style="max-width:120px; float:right;" alt="Autodie" />
-<br/>
-<img src="/img/sponsors/autodie/Newthinking_logoCMYK.png" style="max-width:120px; float:right;" alt="New Thinking" />
-<p>Autodie is a die manufacturing and engineering company located in Grand Rapids, Michigan.</p>
+<h3 class="spon">Lowell Area Schools</h3>
+<p>Thanks to Lowell Schools for giveing us a work area to build are robot!</p>
 <br>
 <h3 class="spon">White's Bridge Tooling</h3>
 <p>For over 25 years, White's Bridge Tooling, Inc., based in Lowell, MI, has turned unique manufacturing problems into custom solutions.</p>
 <br>
-<h3 class="spon">Riverside CNC</h3>
-<p>A shop that creates high-end products with CNC.</p>
+<h3 class="spon">Enworks</h3>
+<p>A company that makes office furniture for offices.</p>
 <h3 class="spon">Perceptive Concepts</h3>
 <p>Perceptive Concepts Design Macanical machiner for manafacur compines like Conveyors,Product elevators,Filling equipment,Palletizing systems (robotic and traditional),Robotic welding and / or assembly systems,Assembly fixtures,Vibratory bowl feed high speed assembly equipment
 Palletized power and free BOSCH / LANCO style systems "Automation by design".</p>
+<br>
+<h3 class"spon">Bernards Ace Hardware</h3>
+<p>they are a hardware store hear in lowell they have donated tools to us and have given us discounts in the past</p>
+<br>
+<h3 class"spon">Metric Manufacturing</h3>
+<p>they do cad dising and manufactring jobs</p>
+<br>
+<h3 class"spon"<doners</h3>
+<p>Deb Ellinger</P>
+<br>
+<h3 class"spon"<Orchard Hills Golf Course</h3>
+<p>they are a golf course loacated in  Shelbyville, MI </p>
 
 <?php
 require_once('/bin/foot.php');
