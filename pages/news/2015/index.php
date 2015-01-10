0f703bb29105e69e7bb376a511210380e8fdccfa
maksymetz <maksymetz@hotmail.com>
2015-01-10 15:03:54 -0500

    Adding 2015 News

---
+++ pages/news/2015/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once('/bin/head.php');
+?>
+
+<!-- **** MAIN AREA **** -->
+<div class="titlebanner">
+<h2 id="newstitle">2015 Season</h2>
+</div>
+
+
+
+
+<?php
+require_once('/bin/foot.php');
+?>
