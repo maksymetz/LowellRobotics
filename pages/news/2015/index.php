f81aad933be9cd95575a37332660c1d22c94050a
Michael Maksymetz <maksymetz@users.noreply.github.com>
2015-01-12 22:15:29 -0500

    Update index.php

--- pages/news/2015/index.php
+++ pages/news/2015/index.php
@@ -6,7 +6,7 @@ require_once('/bin/head.php');
 <div class="titlebanner">
 <h2 id="newstitle">2015 Season</h2>
 </div>
-<iframe src="http://lowellrobotics2015.blogspot.com/"></iframe>
+<iframe src="http://lowellrobotics2015.blogspot.com/" width="700" height="1000"></iframe>
 
 
 
