4ca0328f363847b620ee05c6613544fe1a41471a
Michael Maksymetz <maksymetz@users.noreply.github.com>
2015-01-19 19:30:40 -0500

    Update foliogallery.php
    Turned off captions and random display

--- pages/photogallery/foliogallery.php
+++ pages/photogallery/foliogallery.php
@@ -13,8 +13,8 @@ $itemsPerPage        = '12';    // number of images per page if not already spec
 $thumb_width         = '150';   // width of thumbnails in pixels
 $sort_albums_by_date = TRUE;    // TRUE will sort albums by upload date, FALSE will sort albums by name 
 $sort_images_by_date = TRUE;    // TRUE will sort thumbs by creation date, FALSE will sort images by name 
-$random_thumbs       = TRUE;    // TRUE will display random thumbnails, FALSE will display the first image from thumbs folders
-$show_captions       = TRUE;    // TRUE will display file names as captions on thumbs inside albums, FALSE will display no captions
+$random_thumbs       = FALSE;    // TRUE will display random thumbnails, FALSE will display the first image from thumbs folders
+$show_captions       = FALSE;    // TRUE will display file names as captions on thumbs inside albums, FALSE will display no captions
 $num_captions_chars  = '20';    // number of characters displayed in album and thumb captions
 /***** end gallery settings *****/
 
