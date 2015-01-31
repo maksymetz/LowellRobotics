5584977596fe6d86e9b391529c51538a966f092c
maksymetz <maksymetz@hotmail.com>
2015-01-31 10:50:08 -0500

    Adding Photos

--- pages/about/robot/index.php
+++ pages/about/robot/index.php
@@ -12,6 +12,8 @@ require_once('/bin/head.php');
 <ul>
 <img src="img/image002.jpg" class="newspic" style="max-width:150px;" alt="Page Under Construction!" />
 <li>This year’s goal was to launch a large exercise ball into a goal. To launch the ball we had a catapult-like mechanism powered by motors and chains. The drive train was composed of four omni wheels and two traction wheels. This robot brought us to second place at Gull Lake.</li>
+<br>
+<br>
 </ul>
 
 <div class="titlebanner">
@@ -23,13 +25,29 @@ require_once('/bin/head.php');
 </ul>
 
 <div class="titlebanner">
+<h2 id="newsitem">2012 Robot</h2>
+</div>
+<ul>
+<img src="img/2012Robot.JPG" class="newspic" style="max-width:150px;" alt="Page Under Construction!" />
+<li>
+The game was Rebound Rumble and it consisted of shooting basketballs into a basket and then balance on a bridge in the middle of the field in the last 30 seconds of the match. We won the Traverse City District with 4003 from Allendale and 3509 from Nilas Michigan. We also won the Engineering Excellence award, then Quality Award at the West Michigan District competition, finally we ended our season at the Michigan State Championship. This year’s robot consisted of a 4 wheel drive, a conveyor belt, a roller to pick up the basket balls, a mechanism to lower the bridge.
+</li>
+</ul>
+
+<div class="titlebanner">
 <h2 id="newsitem">2011 Robot</h2>
 </div>
 <ul>
 <img src="img/image006.jpg" class="newspic" style="max-width:150px;" alt="Page Under Construction!" />
 <li>This year’s goal was to place inflatable’s on a scoring rack. The main drive wheels are in the center of the frame and the omni-directional wheels are on the front and back. Our arm is set up with an acme screw powered by a window motor to control the main pivot and a fisher price motor with a modified gearbox to control the secondary pivot. Our minibot is powered by the two motors that came in the Tetrix kit. The wheels are 5 inch diameter machined aluminum with high friction material. The minibot is elevated on a platform to get it up to the 18 inch limit for deployment. This robot took us to states.</li>
 </ul>
-
+<div class="titlebanner">
+<h2 id="newsitem">2010 Robot</h2>
+</div>
+<ul>
+<img src="img/2010Robot.jpg" class="newspic" style="max-width:150px;" alt="Page Under Construction!" />
+<li>This year’s game was Breakaway. The robots had to play a  version of soccer and then climb onto an elevated platform. This was our rookie year, in which we were very successful. We were the highest rookie seed in the West Michigan Curie Division, State Championship as well as the winner of Rookie All Star Rookie Award at West Michigan District. We were also finalist at the 2010 West Michigan Robotics Invitational. The robot consisted of kicker to kick the ball, and a fisher price motor for to pull us up on the tower!</li>
+</ul>
 
 <?php
 require_once('/bin/foot.php');
