144d49d7477f064fc8ef5e519f126200395fd264
braytongrant <braytongrant@gmail.com>
2015-01-31 10:33:11 -0500

    Cysticfibrosis drop down

--- bin/head.php
+++ bin/head.php
@@ -104,6 +104,7 @@ echo('
         <a href="/pages/about/team">Our Team</a>
         <a href="/pages/about/sponsors">Our Sponsors</a>
         <a href="/pages/about/robot">Our Robots</a>
+        <a href="/pages/about/Cysticfibrosis">Cystic Fibrosis</a>
       </div>
     </td>
 	<td style="border-right:#000000 1px solid;">
