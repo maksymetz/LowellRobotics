f8bfcfe655839e97a5ee380ac8360d9431868816
braytongrant <braytongrant@gmail.com>
2015-01-31 11:26:06 -0500

    Start Cystic Fibrosis page

---
+++ pages/about/Cysticfibrosis/Index.php
@@ -0,0 +1,14 @@
+<?php
+$title="Lowell FIRST Robotics Team 3234";
+require_once('/bin/head.php');
+?>
+
+<!-- **** MAIN AREA **** -->
+<h1>Cystic Fibrosis</h1>
+<p>WHAT IS CYSTIC FIBROSIS?                   
+ 
+CF (Cystic Fibrosis) is a genetic disease that causes the body to produce abnormally thick, sticky mucus leading to life-threatening lung infections. By investing in CF research, you are investing in improving the lives of those with the disease. Money buys science and science brings life. Through HFC (Hunt for a Cure) we can give the children and adults with CF the quality of life and future they deserve.</p>
+
+<p>What it's like living with Cystic Fibrosis?
+
+</p>
