5cdfda0926248eda3b27e652a39e053ea871d493
braytongrant <braytongrant@gmail.com>
2015-01-31 11:39:19 -0500

    Hunt for cure link

--- pages/about/Cysticfibrosis/Index.php
+++ pages/about/Cysticfibrosis/Index.php
@@ -7,7 +7,7 @@ require_once('/bin/head.php');
 <h1>Cystic Fibrosis</h1>
 <p>WHAT IS CYSTIC FIBROSIS?                   
  
-CF (Cystic Fibrosis) is a genetic disease that causes the body to produce abnormally thick, sticky mucus leading to life-threatening lung infections. By investing in CF research, you are investing in improving the lives of those with the disease. Money buys science and science brings life. Through HFC (Hunt for a Cure) we can give the children and adults with CF the quality of life and future they deserve.</p>
+CF (Cystic Fibrosis) is a genetic disease that causes the body to produce abnormally thick, sticky mucus leading to life-threatening lung infections. By investing in CF research, you are investing in improving the lives of those with the disease. Money buys science and science brings life. Through HFC (<a href="http://www.huntforacure.com/" target="_blank">Hunt for a Cure</a>) we can give the children and adults with CF the quality of life and future they deserve.</p>
 
 <p>What it's like living with Cystic Fibrosis?
 
