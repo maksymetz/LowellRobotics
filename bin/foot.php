fbd42bf589ff68f5d43ce172b9e16cabbcb74087
BrendanMPhilo <brendanp2001@gmail.com>
2015-02-02 18:53:32 -0500

    Removing About Website Link

--- bin/foot.php
+++ bin/foot.php
@@ -152,7 +152,7 @@ echo('
   </tr>
 </table>
 <hr />
-<span style="font-family:arial;font-size:12px;color:#d0d0d0;text-align:center;">This website is entirely student designed and maintained.  <a href="/pages/about/website">Read more</a><br />
+<span style="font-family:arial;font-size:12px;color:#d0d0d0;text-align:center;">This website is entirely student designed and maintained. <br />
 <a href="mailto:contact@lowellrobotics.org">Email us at contact@lowellrobotics.org</a>
 <br>
 Copyright &#169; '.date("Y").' Lowell FIRST Robotics Team 3234</span>
