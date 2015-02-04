184a70a73c1afdac5c1b51ae933a96e96d941812
maksymetz <maksymetz@hotmail.com>
2015-02-04 18:36:48 -0500

    Test logo pages

---
+++ indexBlack.php
@@ -0,0 +1,72 @@
+<?php
+$title="Red Arrow Robotics";
+require_once('bin/headBlack.php');
+?>
+
+<!-- **** MAIN AREA **** -->
+<h1>Red Arrow Robotics Team 3234</h1>
+
+<img src="/common/team2013-14.jpg" width=400 height=300 alt="Team" class="indexpic" />
+You’ve found your way to the website of FIRST Robotics Team #3234, otherwise known as the Lowell Robotics Team! We participate in the annual FIRST Robotics Competition (FRC), and a member of the FIRST (For Inspiration and Recognition of Science and Technology) organization. Hailing from Lowell, Michigan, we are always looking for new members, and any interested are welcome to drop in on a meeting. Meetings take place Mondays at the Lowell Middle School from six to eight pm every week. For more information, explore the website, which is entirely student-designed and maintained.
+
+<br />
+
+<hr />
+
+<div class="titlebanner">
+<h2 id="newstitle">What's new!</h2>
+</div>
+
+<p>Check out our new:
+<li><a href="https://www.youtube.com/channel/UCA9qOPuNsPEPL-eVTb93nig" target="_blank">Youtube Channel</a></li>
+<li><a href="http://lowellrobotics.org/pages/photogallery/" target="_blank">Photo Gallery (Work in progress)</a></li>
+<li><a href="http://lowellrobotics2015.blogspot.com/" target="_blank">Blog</a></li></p>
+
+<div class="titlebanner">
+<h2 id="newstitle">Contact Us</h2>
+</div>
+
+Lowell Red Arrow Robotics c/o Cindy Brenk, Finance Manager
+<br>300 High Street
+<br>Lowell, MI 49331</br>
+<a href="mailto:contact@lowellrobotics.org">Email us: contact@lowellrobotics.org</a>
+
+<div class="titlebanner">
+<h2 id="newstitle">First Competition at Kentwood</h2>
+</div>
+
+<p>We are currently preparing for our first event at East Kentwood High School on March 6th and 7th. 
+<br>The address is: </br>
+<br>East Kentwood High School 
+<br>6230 Kalamazoo
+<br>Kentwood, MI  49508.</br>
+<br>The schedule is found <a href="http://www.usfirst.org/uploadedFiles/Robotics_Programs/FRC/Events/2015/2015_MIKEN_Agenda.pdf" target="_blank">[here]</a>.  We hope to see you there!</br></p>
+
+
+<div class="titlebanner">
+<h2>Quick Links</h2>
+</div>
+<table>
+  <tr>
+    <td class="xlink">
+      <a href="http://www.usfirst.org" target="_blank"><img src="/bin/pic.php?src=/common/ql_firstlogo.png&amp;width=140&amp;height=140" alt="FIRST" class="quickLinks"/></a><br/>
+      <a href="http://www.usfirst.org" target="_blank">FIRST</a>
+    </td>
+    <td class="xlink">
+      <a href="http://www.firstinmichigan.us" target="_blank"><img src="/bin/pic.php?src=/common/ql_firstinmichigan.png&amp;width=140&amp;height=140" alt="First in Michigan" class="quickLinks"/></a><br/>
+      <a href="http://www.firstinmichigan.us" target="_blank">FIRST in Michigan</a>
+    </td>
+    <td class="xlink">
+      <a href="http://www.thebluealliance.com" target="_blank"><img src="/bin/pic.php?src=/common/ql_bluealliance.png&amp;width=140&amp;height=140" alt="The Blue Alliance" class="quickLinks"/></a><br/>
+      <a href="http://www.thebluealliance.com" target="_blank">The Blue Alliance</a>
+    </td>
+    <td class="xlink">
+      <a href="http://www.chiefdelphi.com" target="_blank"><img src="/bin/pic.php?src=/common/ql_chiefdelphi.png&amp;width=140&amp;height=140" alt="Chief Delphi" class="quickLinks"/></a><br/>
+      <a href="http://www.chiefdelphi.com" target="_blank">Chief Delphi</a>
+    </td>
+  </tr>
+</table>
+
+<?php
+require_once('bin/foot.php');
+?>
