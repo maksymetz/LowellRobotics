2ec9e921c1f0617380036dab4302ba4467425106
braytongrant <braytongrant@gmail.com>
2015-02-09 19:42:15 -0500

    text padding

--- pages/about/sponsors/index.php
+++ pages/about/sponsors/index.php
@@ -6,51 +6,51 @@ require_once('/bin/head.php');
 <!-- **** MAIN AREA **** -->
 <h1 class "spon">Our Sponsors</h1>
 <br>
-<h3 class "spon">Lowell Community Fund <a id="float" href="http://www.grfoundation.org/lowell"><img src="img/lowellCommFund.gif" style="max-width:225px;" alt="Lowell Community Fund" /></a></h3>
+<h3 class "spon"><p id="textpadding">Lowell Community Fund</p> <a id="float" href="http://www.grfoundation.org/lowell"><img src="img/lowellCommFund.gif" style="max-width:225px;" alt="Lowell Community Fund" /></a></h3>
 
 <p>The Lowell Area Community Fund gives grants to organizations that assist in fulfilling its mission to assure community cooperation and participation that supports a healthy, dynamic community.</p>
 <br>
-<h3 class "spon">Lowell Area Schools <a id="float" href="http://www.lowellschools.com/" target="_blank"><img src="img/LOWELL-SCHOOLS-LOGO.jpeg" style="max-width:200px;" alt="Lowell Area Schools" /></a></h3>
+<h3 class "spon"><p id="textpadding">Lowell Area Schools </p><a id="float" href="http://www.lowellschools.com/" target="_blank"><img src="img/LOWELL-SCHOOLS-LOGO.jpeg" style="max-width:200px;" alt="Lowell Area Schools" /></a></h3>
 
 <p>Thanks to Lowell Schools for giving us a work area to build our robot!</p>
 <br>
 <br>
-<h3 class "spon">White's Bridge Tooling <a id="float" href="http://www.wbtooling.com/"><img src="img/whitesbridge.jpg" target="_blank" style="max-width:187px;" alt="White\'s Bridge Tooling" /></a></h3>
+<h3 class "spon"><p id="textpadding">White's Bridge Tooling </p><a id="float" href="http://www.wbtooling.com/"><img src="img/whitesbridge.jpg" target="_blank" style="max-width:187px;" alt="White\'s Bridge Tooling" /></a></h3>
 
 <p>For over 25 years, White's Bridge Tooling, Inc., based in Lowell, MI, has turned unique manufacturing problems into custom solutions.</p>
 <br>
-<h3 class "spon">Enworks <a id="float" href="http://enwork.com/" target="_blank"><img src="img/enwork-logo.jpg" style="max-width:187px;" alt="Enwork" /></a></h3>
+<h3 class "spon"><p id="textpadding">Enworks</p> <a id="float" href="http://enwork.com/" target="_blank"><img src="img/enwork-logo.jpg" style="max-width:187px;" alt="Enwork" /></a></h3>
 
 <p>A company that makes office furniture for offices.</p>
 <br>
-<h3 class"spon">Perceptive Concepts <a id="float" href="http://www.perceptiveconcepts.com/" target="_blank"><img src="img/pci banner.GIF" style="max-width:250px;" alt="Perceptive Concepts" /></a></h3>
+<h3 class"spon"><p id="textpadding">Perceptive Concepts</p> <a id="float" href="http://www.perceptiveconcepts.com/" target="_blank"><img src="img/pci banner.GIF" style="max-width:250px;" alt="Perceptive Concepts" /></a></h3>
 
 <p>Perceptive Concepts designs mechanical machinery like conveyors, product elevators, filling equipment, palletizing systems (robotic and traditional), robotic welding and / or assembly systems and assembly fixtures.</p>
 <br>
-<h3 class "spon">Bernards Ace Hardware <a id="float" href="http://www.acehardware.com/" target="_blank"><img src="img/Ace-_Hardware_logo.jpg" style="max-width:187px;" alt="Ace Hardware" /></a></h3>
+<h3 class "spon"><p id="textpadding">Bernards Ace Hardware </p><a id="float" href="http://www.acehardware.com/" target="_blank"><img src="img/Ace-_Hardware_logo.jpg" style="max-width:187px;" alt="Ace Hardware" /></a></h3>
 
 <p>This hardware store donates tools and offers us discounts.</p>
 <br>
 <br>
-<h3 class "spon">Metric Manufacturing <a id="float" href="http://metricmfg.com/" target="_blank"><img src="img/metric logo.jpg" style="max-width:187px;" alt="Metric Manufacturing" /></a></h3>
+<h3 class "spon"><p id="textpadding">Metric Manufacturing</p> <a id="float" href="http://metricmfg.com/" target="_blank"><img src="img/metric logo.jpg" style="max-width:187px;" alt="Metric Manufacturing" /></a></h3>
 
 <p>They do CAD design and manufacturing.</p>
 <br>
 <br>
-<h3 class "spon">Solidworks <a id="float" href="http://www.solidworks.com/" target="_blank"><img src="img/newredtranslogo.png" style="max-width:187px;" alt="Solidworks" /></a></h3>
+<h3 class "spon"><p id="textpadding">Solidworks </p><a id="float" href="http://www.solidworks.com/" target="_blank"><img src="img/newredtranslogo.png" style="max-width:187px;" alt="Solidworks" /></a></h3>
 
 <p>Solidworks Corp. offers complete 3D software tools that let you create, simulate, publish, and manage your data.</p>
 <br>
-<h3 class "spon">Orchard Hills Golf Course <a id="float" href="http://www.orchardhillsgc.com/" target="_blank"><img src="img/orchardhillsgolfcourse.jpg" style="max-width:187px;" alt="Enwork" /></a></h3>
+<h3 class "spon"><p id="textpadding">Orchard Hills Golf Course </p><a id="float" href="http://www.orchardhillsgc.com/" target="_blank"><img src="img/orchardhillsgolfcourse.jpg" style="max-width:187px;" alt="Enwork" /></a></h3>
 
 <p>Shelbyville, MI.</p>
 <br>
 <br>
-<h3 class "spon">Trombly Chiropractic <a id="float" href="http://tromblychiropractic.com/" target="_blank"><img src="img/Trombly Chiropractic.jpg" style="max-width:325px;" alt="Trombly Chiropractic" /></a></h3>
+<h3 class "spon"><p id="textpadding">Trombly Chiropractic</p> <a id="float" href="http://tromblychiropractic.com/" target="_blank"><img src="img/Trombly Chiropractic.jpg" style="max-width:325px;" alt="Trombly Chiropractic" /></a></h3>
 
 <p>Lowell, MI</p>
 <br>
-<h3 class "spon">Family Dentistry of Lowell <a id="float" href="http://www.familydentistryoflowell.com/" target="_blank"><img src="img/LFDlogo_address.gif" style="max-width:325px;" alt="Family Dentistry of Lowell" /></a></h3>
+<h3 class "spon"><p id="textpadding">Family Dentistry of Lowell</p> <a id="float" href="http://www.familydentistryoflowell.com/" target="_blank"><img src="img/LFDlogo_address.gif" style="max-width:325px;" alt="Family Dentistry of Lowell" /></a></h3>
 
 <p>Lowell, MI</p>
 <br>
