6411f71e6e7276621246b984f783d784db9099cf
maksymetz <maksymetz@hotmail.com>
2015-02-09 19:04:34 -0500

    Updating counter and about team

--- pages/about/team/index.php
+++ pages/about/team/index.php
@@ -11,15 +11,19 @@ require_once('/bin/head.php');
 </div>
 <p>In 2008, a student approached Al Eckman to request he be the head mentor for a FRC robotics team. He agreed and applied for a grant from the Lowell Community Fund.  The funds were used the first year to gain experience with smaller robots.  The following year we moved forward in the FIRST program, graduating to larger robots. </p>
 
-<div class="titlebanner"><h2>Our Vision:</h2></div>
-<p>To foster the recognition and celebration of science, engineering, and technology, enabling our  youth to become the heroes of tomorrow.</p>
+<div class="titlebanner"><h2>Purpose</h2></div>
+<p>To increase the understanding of science, engineering, and technology in everyday life while building self-confidence, communication, and leadership skills in local youth through robotics and community involvement.</p>
 
-<div class="titlebanner"><h2>Our Mission:</h2></div>
-<p>To increase understanding and spark interest in science, technology and engineering, in everyday life while building self-confidence, communication, teamwork and leadership skills in students through FRC and community involvement.</p>
+<div class="titlebanner"><h2>Vision</h2></div>
+<p>To become a world class team in the FIRST program and enable our students to be the STEM leaders of their generation.</p>
+
+<div class="titlebanner"><h2>Mission</h2></div>
+<p>Develop a team that will design and build competitive robots, educate the community in STEM, and become self-sustaining through a business program and community sponsorship.</p>
 
 <div class="titlebanner"><h2>Community Involvement</h2></div>
 <h2 class="spon">Alto Fall Festival</h2>
 <p>A celebration in Alto, a village south of Lowell, that we have taken part in. When we did, we helped with the Library's book sale and had a demonstration of the robot.</p>
+
 <h2 class="spon">Freshman Orientation</h2>
 <p>Incoming freshman and their parents come to the high school to learn about the various activities they can take part in.</p>
 <h2 class="spon"><a href="http://www.huntforacure.com/" target="_blank">Hunt for a Cure</a></h2>
