0a6bc3da37530ff77e60b37f0ced11123f85796b
mark <mew2011@live.com>
2015-12-01 18:23:56 -0500

    Update external_links.php
    Us first.org is not used anymore by first

--- pages/links/external_links.php
+++ pages/links/external_links.php
@@ -9,8 +9,8 @@ require_once('/bin/head.php');
 <table>
   <tr>
     <td class="xlink">
-      <a href="http://www.usfirst.org" target="_blank"><img src="/common/ql_firstlogo.png" alt="FIRST" style="height:140px;width:140px;border:#990000 2px solid;" /></a>
-      <a href="http://www.usfirst.org" target="_blank">www.USFIRST.org</a>
+      <a href="http://http://www.firstinspires.org" target="_blank"><img src="/common/ql_firstlogo.png" alt="FIRST" style="height:140px;width:140px;border:#990000 2px solid;" /></a>
+      <a href="http://http://www.firstinspires.org" target="_blank">http://www.firstinspires.org</a>
     </td>
     <td class="xlink">
       <a href="http://www.firstinmichigan.us" target="_blank"><img src="/common/ql_firstinmichigan.png" alt="First in Michigan" style="height:140px;width:140px;border:#990000 2px solid;" /></a>
