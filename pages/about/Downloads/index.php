ecf9b68dd55ef707ddec0dc6b7db3918f8f19461
Michael Maksymetz <maksymetz@hotmail.com>
2016-01-13 20:34:57 -0500

    Pictures and Downloads

--- pages/about/Downloads/index.php
+++ pages/about/Downloads/index.php
@@ -6,15 +6,21 @@ require_once('/bin/head.php');
 <!-- **** MAIN AREA **** -->
 <h1>Team 3234 Downloads</h1>
 <p>Downloads for team members, parents and fans.</p>
-<div class="titlebanner">
-<h2 id="newstitle">For Parents</h2>
-</div>
-<p><a href="http://www.firstinmichigan.org/Documents/2014_Michigan_Consent_and_Release_Form.pdf?file=2014_Michigan_Consent_and_Release_Form" target="_blank">2014 Michigan Consent and Release Form</a></p>
 
-<div class="titlebanner" target="_blank"><h2>Source Code</h2></div>
+<div class="titlebanner"><h2>2016 Season</h2></div>
+<p><a href="Files\frc-2016-gamemanual.pdf">2016 Game Manual</a></p>
+<p><a href="http://www.firstinspires.org/sites/default/files/uploads/resource_library/frc/game-and-season-info/competition-manual/2016/FRC-2016-admin-manual.pdf">2016 Admin Manual</a></p>
+
+<div class="titlebanner"><h2>2015 Source Code</h2></div>
 <p><a href="Files\it_rar.zip">2015 Robot</a></p>
 <p><a href="Files\android_scouting_2015.zip">Android scouting Program</a></p>
 <p><p><a href="Files\scout2015consolidate.zip">Scouting Consolidation Program in Visual Studio</a></p>
+
+<div class="titlebanner"><h2>For Parents</h2>
+</div>
+<p><a href="http://www.firstinmichigan.org/Documents/2014_Michigan_Consent_and_Release_Form.pdf?file=2014_Michigan_Consent_and_Release_Form" target="_blank">2014 Michigan Consent and Release Form</a></p>
+
+
 
 <?php
 require_once('/bin/foot.php');
